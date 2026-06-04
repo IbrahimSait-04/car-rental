d58636f9b752c7f78db11d2fecca461db140048a
Ibrahim <saitibrahimsait8369@gmail.com>
2026-06-04 12:36:06 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { toggleTheme } from '../redux/themeSlice';
+import { toggleTheme } from '../Redux/ThemeSlice';
 
 const Navbar = () => {
   const theme = useSelector((state) => state.theme.value);
