975598738b55f59fe27f288a04d0c4d3464b772c
Ibrahim <saitibrahimsait8369@gmail.com>
2026-06-04 12:48:24 +0530

    updated codes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { removeFromCart , clearcart} from '../redux/cartSlice'
+import { removeFromCart , clearcart} from '../Redux/CartSlice'
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.value); // get cars from redux
